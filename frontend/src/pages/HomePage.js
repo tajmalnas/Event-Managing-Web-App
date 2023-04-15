d26bf13dfb1c7e03b1430343e0c3a4a80ca9425e
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-15 14:07:27 +0530

    added homepage component and succesfully completed the web app

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import PageContent from '../components/PageContent'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <PageContent title="Welcome!">
+      <p>Browse our all Events</p>
+    </PageContent>
   )
 }
 
